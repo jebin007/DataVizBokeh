c1da4c9ce0890def842324f4a4df0ee187473242
jebin007 <jevinsubedi@gmail.com>
2017-08-05 11:45:01 -0500

    Plotting Life-Expectancy Vs Population of Various Continents : -removed the 5 country limit -added a color mapper to represent each continent -added a legend, it's background color and location.

--- stage1-3.py
+++ stage1-3.py
@@ -3,16 +3,25 @@ import pandas as pd
 
 from bokeh.io import output_file, show
 from bokeh.plotting import ColumnDataSource, figure
+from bokeh.models import CategoricalColorMapper
 
 output_file('pop-life.html')
 
 file = 'country-pops.csv'
 
-countries = pd.read_csv(file, nrows=5)      #nrows=5 limits the data being pulled out to just 5 rows for simplicity
+countries = pd.read_csv(file)      #nrows=5 limits the data being pulled out to just 5 rows for simplicity
 
 country_data = ColumnDataSource(countries)
 
+color_mapper = CategoricalColorMapper(factors=['Asia','Africa','Antarctica','Australia','Central America','Europe','North America', 'Oceania', 'South America'],
+                                      palette=['#00FF00', '#FFD343', 'darkgrey', 'brown', 'cyan', 'crimson', 'red', '#0000FF', 'purple']
+                                      )
+
 plot = figure(x_axis_label='Population', y_axis_label='Life Expectancy')
-plot.circle(x='Population', y='Life_expectancy', source=country_data, size=15)          #x and y values are column headers in the csv file stored inside the variable country_data
+plot.diamond(x='Population', y='Life_expectancy', source=country_data, size=10, color=dict(field='Continent', transform=color_mapper), legend='Continent')          #x and y values are column headers in the csv file stored inside the variable country_data
+
+plot.legend.location = 'bottom_right'
+plot.legend.background_fill_color = 'lightgrey'
+
 
 show(plot)
