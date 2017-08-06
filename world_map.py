18ade9728742d52876d4e2326900356f7f5ac7d5
jebin007 <jevinsubedi@gmail.com>
2017-08-06 02:11:25 -0500

    World Map With Hover Id: -merged latitudes and longitudes to the dataframe of the csv file. -used the merged dataframe(world_map_with_data) as a columndatasource.

--- world_map.py
+++ world_map.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import requests
 
 from bokeh.io import output_file, show
@@ -42,16 +43,51 @@ def get_coordinates(features):
 
 country_id, lats, longs = get_coordinates(json_data['features'])
 
+country_coords = []
+
+#populate country_coords with latitudes and longitudes
+for index, country in enumerate(country_id):
+    land_mass = {'country_code': country, 'latitudes': lats[index],
+                 'longitudes': longs[index]
+                 }
+    country_coords.append(land_mass)
+
+country_maps = pd.DataFrame(country_coords)
+
+input_file = 'country-pops.csv'
+country_populations = pd.read_csv(input_file)
+
+#this merges the data from the csv by adding a new column of latitudes and longitudes
+world_map_with_data = country_populations.merge(country_maps,
+                                                left_on='ISO_3166-1_alpha3',
+                                                right_on='country_code',
+                                                how="outer"
+                                                )
+
+#convert world_map_with_data dataframe to ColumnDataSource.
+map_data = ColumnDataSource(world_map_with_data)
+
 world_map_plot = figure(plot_width=900, plot_height=600, title='World Map', tools=TOOLS,
                         x_range=(-180, 180),
                         y_range=(-90, 90)
                         )
 
 #lats and longs here are x and y coordinates
-world_map_plot.patches(lats, longs,
+world_map_plot.patches(xs='latitudes',
+                       ys='longitudes',
+                       source=map_data,
                        fill_color='#F1EEF6',
                        fill_alpha=0.7,
                        line_width=2
                        )
+
+#We merged country coordinates data with the dataframe from csv files to display relevant hover data at each coordinates.
+hover = world_map_plot.select_one(HoverTool)
+hover.tooltips = [ ('Country Name English', '@Country_English'),
+                   ('Population', '@Population'),
+                   ('Life Expectancy (years)', '@Life_Expectancy'),
+                ]
+
+
 
 show(world_map_plot)
